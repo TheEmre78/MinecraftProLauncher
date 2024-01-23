8bcb177bbf11ae304ac998199708d66e99e0eb89
TheEmre78 <120399408+TheEmre78@users.noreply.github.com>
2024-01-23 09:17:29 +0300

    Update SplashScreen.cs

--- SplashScreen.cs
+++ SplashScreen.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.UI.WebControls;
@@ -13,22 +19,74 @@ namespace RL_RoseLone
             InitializeComponent();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-                loadingbar.Width += 50;
-                if (loadingbar.Width == 600)
-                {
-                    sayac.Stop();
-                    Login giris = new Login();
-                    giris.Show();
-                    Hide();
-                }
-        }
+        private int counter = 0;
 
         private void splashscreen_Load(object sender, EventArgs e)
         {
-            loadingbar.Width = 0;
-            sayac.Start();
+            Timer t = new Timer();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(CheckInternet);
+            t.Start();
+        }
+
+        private void CheckInternet(object sender, EventArgs e)
+        {
+            // İnternet bağlantısını kontrol edin
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                // İnternet bağlantısı varsa, loadingtxt'ye Bağlantı kuruldu yazın
+                loadingtxt.Text = "Bağlantı kuruldu";
+
+                // Timer'ı durdurun
+                (sender as Timer).Stop();
+
+                // Yeni Timer oluşturun ve her 2000 milisaniye (2 saniye) çalışması için ayarlayın
+                Timer t = new Timer();
+                t.Interval = 100;
+                t.Tick += new EventHandler(UpdateUI);
+                t.Start();
+            }
+            else
+            {
+                // İnternet bağlantısı yoksa, loadingtxt'ye Bağlantı kurulamadı yazın
+                loadingtxt.Text = "Bağlantı kurulamadı. Yeniden deneniyor...";
+            }
+        }
+
+        private void UpdateUI(object sender, EventArgs e)
+        {
+            // Loadingbarı 10 arttırın
+            loadingBar.Value = Math.Min(loadingBar.Value + 10, 100);
+
+            // Her 2 saniye, loadingtxt'nin metnini değiştirin
+            counter++;
+            if (counter == 2 || counter == 4 || counter == 6 || counter == 8)
+            {
+                switch (counter)
+                {
+                    case 2:
+                        loadingtxt.Text = "Kontrol Ediliyor...";
+                        break;
+                    case 4:
+                        loadingtxt.Text = "Kontrol Edildi";
+                        break;
+                    case 6:
+                        loadingtxt.Text = "Kontrol Başarılı";
+                        break;
+                    case 8:
+                        loadingtxt.Text = "Başlatılıyor...";
+                        break;
+                }
+            }
+
+            // Loadingbar 100 olduğunda Login formuna geçiş yapın
+            if (loadingBar.Value == 100)
+            {
+                Login loginForm = new Login();
+                loginForm.Show();
+                (sender as Timer).Stop();
+                Hide();
+            }
         }
     }
 }
